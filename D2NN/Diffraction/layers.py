a26c2256e880302a207c5c6c3dfc6b8f127adc05
Vicente <vicentevrocha.vvr@gmail.com>
2023-10-21 17:02:46 +0100

    Corrected an error in the simulation of the Rayleigh-Sommerfeld diffraction where the circular convolution was not being taken into account. This together with the introduction of the Simpson's rule improves the stability of the solver and improves precision. A Q-factor indicating convergence to a valid solution was introduced based on the core manuscript indicated in previous submissions.

--- D2NN/Diffraction/layers.py
+++ D2NN/Diffraction/layers.py
@@ -2,19 +2,96 @@ import torch
 from torch.nn import *
 
 import numpy as np
+from matplotlib.pyplot import *
 
 class RSDiffraction(Module):
-    def __init__(self, z, k, wlen, ds, shape, padding):
+    def __init__(self, z, wlen, ds, input_shape, output_shape, n=1., verbose=0, types=(torch.float32, torch.complex64)):
         super(RSDiffraction, self).__init__()
-
-        self.ds = ds
-
-        self.kernel = torch.fft.fft2(self.OnAxisPointSpreadFunction(z, k, ds, shape), norm="backward")
-        self.kernel = torch.fft.fftshift(self.kernel)
-        #self.kernel = AS(z, k, wlen, ds, shape)
         
-        self.padding = padding
+        self.z = z
+        self.k = n*2*np.pi/wlen
+        self.wlen = wlen
+        self.ds = ds
+        self.input_shape = input_shape
+        self.output_shape = output_shape
+        self.verbose = verbose
+        self.types = types
 
+        self.QualityFactor()
+        
+        self.InitiateLayer()
+                        
+    def NumericalPadding(self):
+        self.numerical_padding = ZeroPad2d((0, self.output_shape[0]-1, 0, self.output_shape[1]-1))
+
+    def DiffractionPadding(self):
+        w_pad = int((self.output_shape[0]-self.input_shape[0])/2)
+        h_pad = int((self.output_shape[1]-self.input_shape[1])/2)
+
+        self.diffraction_padding = ZeroPad2d((w_pad, w_pad, h_pad, h_pad))
+
+    def InitiateLayer(self):
+        self.Kernel()  ## compute kernel
+        self.SimpsonMatrix()  ## compute Simpson Matrix
+        
+        # Padding Layers
+        self.DiffractionPadding()
+        self.NumericalPadding()
+        
+    def QualityFactor(self):
+        r_max = np.sqrt((int(self.output_shape[0]/2 -.5)*self.ds)**2 + (int(self.output_shape[1]/2)*self.ds)**2)
+        ds_ideal = np.sqrt((self.wlen)**2 + r_max**2 + 2*self.wlen*np.sqrt(r_max**2 + self.z**2)) - r_max
+        
+        self.Q_factor = .5*ds_ideal/self.ds
+        
+        if self.verbose is 1:
+            if self.Q_factor > 1.:
+                print(f"Quality Factor Q={self.Q_factor}>1 indicates reliable simulation.")
+            elif self.Q_factor <= 1.:
+                print(f"Quality Factor Q={self.Q_factor}<1 indicates unreliable simulation.")
+
+    def Kernel(self):
+        psf = self.OnAxisPointSpreadFunction()
+        self.kernel = torch.fft.fft2(psf, norm="backward")
+        
+        if self.verbose is 2:
+            fig, axs = subplots(2, 2)
+
+            axs[0,0].set_title(r"$\mathbb{Re}\left[psf\right]$")
+            axs[0,1].set_title(r"$\mathbb{Im}\left[psf\right]$")
+            axs[1,0].set_title(r"$\mathbb{Re}\left[kernel\right]$")
+            axs[1,1].set_title(r"$\mathbb{Im}\left[kernel\right]$")
+            
+            ax = axs[0,0].imshow(psf.real)
+            fig.colorbar(ax, ax=axs[0,0])
+            ax = axs[0,1].imshow(psf.imag)
+            fig.colorbar(ax, ax=axs[0,1])
+            ax = axs[1,0].imshow(self.kernel.real)
+            fig.colorbar(ax, ax=axs[1,0])
+            ax = axs[1,1].imshow(self.kernel.imag)
+            fig.colorbar(ax, ax=axs[1,1])
+            
+            axs[0,0].set_xticks([])
+            axs[0,0].set_yticks([])
+            axs[0,1].set_xticks([])
+            axs[0,1].set_yticks([])
+            axs[1,0].set_xticks([])
+            axs[1,0].set_yticks([])
+            axs[1,1].set_xticks([])
+            axs[1,1].set_yticks([])
+
+    def SimpsonMatrix(self):
+        B = torch.zeros((1, self.input_shape[0]))
+        B[0, ::2] = 4
+        B[0, 1::2] = 2
+        
+        B[0, 0]=1.
+        B[0, -1]=1
+                
+        B = B/3.
+                
+        self.W = torch.matmul(torch.transpose(B, 0, 1), B)
+        
     def PointSpreadFunction(self, x, y, z, k):
         """Point Spread Function in the spatial domain.
 
@@ -29,15 +106,15 @@ class RSDiffraction(Module):
         """
         r_abs = torch.sqrt(x**2 + y**2 + z**2)
 
-        real = torch.tensor([0], dtype=torch.float64)
-        imag = torch.tensor([1], dtype=torch.float64)
+        real = torch.tensor([0], dtype=self.types[0])
+        imag = torch.tensor([1], dtype=self.types[0])
         j = torch.complex(real, imag)
 
-        psf = torch.exp(j * k * r_abs) * z * (1/r_abs - j*k)/(2*np.pi*r_abs**2)
+        psf = (torch.exp(j * k * r_abs) * z * ((1/r_abs) - j*k))/(2*np.pi*r_abs**2)
 
         return psf
 
-    def OnAxisPointSpreadFunction(self, z, k, ds, computation_window_shape):
+    def OnAxisPointSpreadFunction(self):
         """Point Spread Function at the plane z centered around the axis.
 
         Args:
@@ -49,26 +126,38 @@ class RSDiffraction(Module):
         Returns:
             tensor(torch.complex128): Point Spread Function in the spatial domain in the plane at a propagation distance z.
         """
-        xx = torch.arange(-int(computation_window_shape[0]/2)*ds, (1+int(computation_window_shape[0]/2))*ds, ds)
-        yy = torch.arange(-int(computation_window_shape[1]/2)*ds, (1+int(computation_window_shape[1]/2))*ds, ds)
 
-        xx = (xx[1:]+xx[:-1])/2
-        yy = (yy[1:]+yy[:-1])/2
-
-        xx, yy = torch.meshgrid(xx, yy)
+        x = torch.arange(-(self.output_shape[0]/2 -.5)*self.ds, (self.output_shape[0]/2)*self.ds, self.ds).type(self.types[1])
+        y = torch.arange(-(self.output_shape[1]/2 -.5)*self.ds, (self.output_shape[1]/2)*self.ds, self.ds).type(self.types[1])
         
-        psf = self.PointSpreadFunction(xx, yy, z, k)
+        X = torch.zeros(2*self.output_shape[0]-1)
+        Y = torch.zeros(2*self.output_shape[1]-1)
+
+        X[:self.output_shape[0]-1] = float(x[0]) - torch.flip(x[1:], dims=(0,))
+        X[self.output_shape[0]-1:] = x-float(x[0])
+        
+        Y[:self.output_shape[1]-1] = float(y[0]) - torch.flip(y[1:], dims=(0,))
+        Y[self.output_shape[1]-1:] = y-float(y[0])
+        
+        X, Y = torch.meshgrid(X, Y)
+
+        psf = self.PointSpreadFunction(X, Y, self.z, self.k)
 
         return psf
-        
+    
     def Propagate(self, x):
+        x = torch.mul(self.W, x)
+        
+        x = self.diffraction_padding(x)
+        
+        x = self.numerical_padding(x)
+        
         x = torch.fft.fft2(x, norm="backward")
-        x = torch.fft.fftshift(x)
 
         output_fft = torch.mul(self.kernel, x)
 
-        output = torch.fft.ifft2(output_fft, norm="backward")
-        output = torch.fft.ifftshift(output)
+        output = torch.fft.ifft2(output_fft, norm="backward")[:, -self.output_shape[0]:, -self.output_shape[1]:]
+        
         return output*self.ds**2
     
 class IntensityMeasurement(Module):
@@ -77,3 +166,4 @@ class IntensityMeasurement(Module):
 
     def Measure(self, x):
         return torch.abs(x)**2
+
